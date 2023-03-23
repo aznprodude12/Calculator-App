c7dbc56f3be799616753fa093bc1d72aae52a9d3
aznprodude12 <hoa_ho123@yahoo.com>
2023-03-23 16:37:42 -0500

    Digits and Operators Button Click Functionality
    This commit adds the functionality that displays the digits and operators that is clicked.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useState } from 'react';
+
 function App() {
+  const [ calc, setCalc ] = useState("");
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    if (
+      ops.includes(value) && calc === '' ||
+      ops.includes(value) && ops.includes(calc.slice(-1))
+      ) {
+        return;
+      }
+    setCalc(calc + value);
+  }
+
   const createDigits = () => {
     const digits = [];
 
     for (let i = 1; i < 10; i++) {
       digits.push(
-        <button key={i}>{i}</button>
+        <button 
+          key={i} 
+          onClick={() => updateCalc(i.toString())}>
+            {i}
+        </button>
       )
     }
 
@@ -15,20 +35,21 @@ function App() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span> 0
+          <span>(0)</span>&nbsp;
+          { calc || "0" }
         </div>
         <div className="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
 
           <button>DEL</button>
         </div>
         <div className="digits">
           { createDigits() }
-          <button>0</button>
-          <button>.</button>
+          <button onClick={() => updateCalc('0')}>0</button>
+          <button onClick={() => updateCalc('.')}>.</button>
           <button>=</button>
         </div>
       </div>
